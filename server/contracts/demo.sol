44eacbb26acfd054809fe54f1d78bbfa172d080a
jesse uzoma <71229278+officailesse@users.noreply.github.com>
2022-06-18 14:48:35 -0600

    added some files
    update  readme file
    and added a sol

---
+++ server/contracts/demo.sol
@@ -0,0 +1,15 @@
+pragma solc >=0.7.0 <0.9.0;
+
+contract Storage {
+    unint256 number;
+
+    function store(uint256 num ) public {
+   number = num 
+    }
+     
+    
+
+    function retrieve() public view returns (uint256){
+        return 
+    }
+}
